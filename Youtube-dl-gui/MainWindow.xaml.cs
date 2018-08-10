0d869162fa513819bc9a296bd8e0abd39f2788df
Marco Lavoie <marco.lavoie@outlook.com>
2018-08-09 20:51:32 -0400

    Première version fonctionnelle.

--- Youtube-dl-gui/MainWindow.xaml.cs
+++ Youtube-dl-gui/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO;
+
 namespace Youtube_dl_gui
 {
 	/// <summary>
@@ -20,9 +25,78 @@ namespace Youtube_dl_gui
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		string OutputDirectory = Properties.Settings.Default["OutputDirectory"] as string;
 		public MainWindow()
 		{
 			InitializeComponent();
+			OutputDirectoryText.Text = OutputDirectory;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+
+			if (string.IsNullOrEmpty(videoURL.Text))
+			{
+				System.Windows.Forms.MessageBox.Show("Entre l'URL de la vidéo sousplais", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+			else
+			{
+				RunConvert(videoURL.Text);
+			}
+		}
+
+		private bool RunConvert(string url)
+		{
+			if (string.IsNullOrEmpty(url))
+			{
+				System.Windows.Forms.MessageBox.Show("Entre l'URL de la vidéo sousplais", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return false;
+			}
+			else
+			{
+				var cmd = new ProcessStartInfo();
+				cmd.FileName = $@"youtube-dl.exe";
+				cmd.Arguments = $@"--extract-audio --audio-format mp3 {url} -o {OutputDirectory}\%(title)s.%(ext)s";
+				cmd.RedirectStandardInput = true;
+				cmd.RedirectStandardOutput = true;
+				cmd.CreateNoWindow = true;
+				cmd.UseShellExecute = false;
+				cmd.WindowStyle = ProcessWindowStyle.Hidden;
+
+				try
+				{
+					Process.Start(cmd);
+					return true;
+				} catch
+				{
+					return false;
+				}
+			}
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			System.Diagnostics.Process.Start(OutputDirectory);
+		}
+
+		private void Button_Click_2(object sender, RoutedEventArgs e)
+		{
+			using (var fbd = new FolderBrowserDialog())
+			{
+				DialogResult result = fbd.ShowDialog();
+
+				if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					string[] files = Directory.GetFiles(fbd.SelectedPath);
+					Properties.Settings.Default["OutputDirectory"] = fbd.SelectedPath;
+					Properties.Settings.Default.Save();
+					OutputDirectory = fbd.SelectedPath;
+					OutputDirectoryText.Text = OutputDirectory;
+				}
+			}
+
 		}
 	}
 }
